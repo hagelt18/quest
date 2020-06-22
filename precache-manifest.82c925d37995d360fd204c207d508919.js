d0dfed4072775a15d1284c411389acc0b188440c
hagelt18 <hagelt18@gmail.com>
2020-06-21 23:26:50 -0500

    Updates

---
+++ precache-manifest.82c925d37995d360fd204c207d508919.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f34913540703f5ee5a1c251e333acd8",
+    "url": "/quest/index.html"
+  },
+  {
+    "revision": "7339d817d95413bbf5d7",
+    "url": "/quest/static/css/main.3d61913d.chunk.css"
+  },
+  {
+    "revision": "beda05791641da8e805a",
+    "url": "/quest/static/js/2.d5fdc868.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quest/static/js/2.d5fdc868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7339d817d95413bbf5d7",
+    "url": "/quest/static/js/main.08b554be.chunk.js"
+  },
+  {
+    "revision": "71d1c9c3def07bdf583e",
+    "url": "/quest/static/js/runtime-main.ff00ec52.js"
+  },
+  {
+    "revision": "25a6dfbd5765827f46e8ede9f2ad1d1e",
+    "url": "/quest/static/media/chest3d.25a6dfbd.gif"
+  }
+]);
