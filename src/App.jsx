5c5e0aa1ea9e9131f64ebc30f8a5dc5af114fac3
hagelt18 <hagelt18@gmail.com>
2020-07-05 21:55:53 -0500

    Try HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Home from './areas/home';
 import Test from './areas/test';
 
@@ -7,17 +7,17 @@ const ROOT = '/quest';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename='/'>
       <div>
         <Switch>
           {/* <Route path="/" component={Home} strict exact />
         <Route path="/test" component={Test} strict exact /> */}
 
-          <Route path={`${ROOT}/`} exact ><Home /></Route>
-          <Route path={`${ROOT}/test`} exact ><Test /></Route>
+          <Route path={`/`} exact ><Home /></Route>
+          <Route path={`/test`} exact ><Test /></Route>
         </Switch>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
